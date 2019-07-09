3a15afca5bb908b110dd568f5398bc4e0f02c159
hosni <corobonhosni@gmail.com>
2019-07-09 11:51:27 -0430

    add ccsnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
-var path = require('path');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-var open = require('gulp-open');
 var browserSync = require('browser-sync').create();
+var cssnano = require('cssnano')
+var postcss = require('gulp-postcss')
 
 var Paths = {
   HERE: './',
@@ -15,12 +14,28 @@ var Paths = {
   HTML: './*.html'
 };
 
+var postcssPlugins = [
+  cssnano({
+    core: true,
+    zindex: false,
+    autoprefixer: {
+      add: true,
+      browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'
+    }
+  })
+]
+
+
+var sassConfig = {
+  outputStyle: 'expanded',
+}
+
 gulp.task('compile-scss', function() {
   return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write(Paths.HERE))
+    .pipe(sass(sassConfig).on('error', sass.logError))
+    .pipe(postcss(postcssPlugins))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(Paths.CSS))
     .pipe(browserSync.stream());
 });
@@ -36,13 +51,5 @@ gulp.task('server', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(Paths.SCSS, gulp.series('compile-scss'));  
+  gulp.watch(Paths.SCSS, gulp.series('compile-scss'));
 });
-
-gulp.task('open', function(fopen) {
-  gulp.src('index.html')
-    .pipe(open());
-    fopen();
-});
-
-gulp.task('open-app', gulp.series('open', 'watch'));
